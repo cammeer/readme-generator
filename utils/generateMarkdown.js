a9c6bea5668734e9068b79619b34b66d55fa082c
cardinal <caren@merzhaus.org>
2022-06-16 11:52:04 -0400

    question link not working, need to add license stuff

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
 
 
 //checks to generate if any fields are empty, which is ok
@@ -84,7 +84,7 @@ function generateMarkdown(data) {
   * This application is covered under the ${data.license} license
   ${generateContribute(data.contributing)}
   ${generateTesting(data.testing)}
-  ## Any Questions?
+  ## Any Questions
   This project was created by me: [${data.githubUsername}](${data.githubLink})
   
   Contact me with any questions at: [${data.email}](${data.email})
